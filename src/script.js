7bd5952ca706e5c59a33eb21d044ea0f66ed682b
Sebastianmoldrup <sebastian.moldrup@gmail.com>
2022-08-18 15:45:33 +0200

    fixing

---
+++ src/script.js
@@ -0,0 +1,58 @@
+const input = document.getElementById(`input`);
+const output = document.getElementById(`output`);
+const addbtn = document.getElementById(`addbtn`);
+
+// Event listener for button to be clicked to add a Todo
+addbtn.addEventListener(`click`, addTodo);
+
+// Function for adding the Todo item
+function addTodo(e) {
+
+    if(input.value === "") {
+        window.alert("You have to write something!")
+        return;
+    }
+
+    // If the event doesnt get handled correctly it wont execute by default
+    //e.preventDefault();
+
+    // Adding DOM elements for the Todo Items
+    const todoDiv = document.createElement(`div`);
+    const todoItem = document.createElement(`span`);
+    const deleteBtn = document.createElement(`button`);
+
+    // Adding text to the todoItem and deleteBtn
+    todoItem.innerText = input.value;
+    deleteBtn.innerText = "Remove";
+
+    // Adding tailwindcss with class to the DOM elements
+    todoDiv.classList.add("flex", "flex-row", "gap-5", "justify-between", "py-1");
+    todoItem.classList.add("text-white", "p-1");
+    deleteBtn.classList.add("text-white", "border-2", "border-red-600", "rounded-xl", "p-1", "hover:border-white", "hover:text-red-400");
+
+    // Appending the DOM elements
+    todoDiv.appendChild(todoItem);
+    todoDiv.appendChild(deleteBtn);
+    output.appendChild(todoDiv);
+
+    // Resetting the input when adding a todo item
+    input.value = "";
+
+    // Adding a event listener for the deleteBtn to run the deleteTodo function
+    deleteBtn.addEventListener('click', deleteTodo);
+}
+
+
+
+// Function for deleting a Todo Item
+function deleteTodo(e) {
+
+    // Making a variable to store the event target
+    const item = e.target;
+
+    // Making a variable to store the event target parent element
+    const todo = item.parentElement;
+
+    // Removing the entire Todo by deleting the parent element
+    todo.remove();
+}
